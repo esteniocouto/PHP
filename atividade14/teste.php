72e19eb3b645b0e76a2526de9a3e51596a0db58d
ESTENIO COUTO BORGES JUNIOR <esteniojr.ti@gmail.com>
2022-11-29 22:27:28 -0300

    calculadora

---
+++ atividade14/teste.php
@@ -0,0 +1,37 @@
+<!DOCTYPE HTML>
+<html lang = "pt-br">
+<head>
+   <title>Exemplo</title>
+   <meta charset = "UTF-8">
+</head>
+<body>
+   <form action="" method="post" >
+      Primeiro Numero: <input name="num1" type="text"><br>
+      Segundo numero: <input name="num2" type="text"><br>
+      <input type="submit" name="operacao" value="+">     
+      <input type="submit" name="operacao" value="-">     
+      <input type="submit" name="operacao" value="*">     
+      <input type="submit" name="operacao" value="/">     
+   </form> 
+<?php
+
+   $a = $_POST['num1'];
+   $b = $_POST['num2'];
+   $op= $_POST['operacao'];
+
+   if( !empty($op) ) {
+      if($op == '+')
+         $c = $a + $b;
+      else if($op == '-')
+         $c = $a - $b;
+      else if($op == '*')
+         $c = $a*$b;
+      else
+         $c = $a/$b;
+
+      echo "O resultado da opera&ccedil;&atilde;o &eacute;: $c";
+   }
+
+?>       
+</body>
+</html>
